d2d998b16016f6637a5bc01c726da908eb66086e
Rene Saarsoo <nene@triin.net>
2020-09-21 16:38:10 +0300

    Workout header parsing tests

--- src/parser/parser.test.ts
+++ src/parser/parser.test.ts
@@ -10,4 +10,38 @@ describe("Parser", () => {
       `"Workout is missing a name. Use \`Name:\` to declare one."`
     );
   });
+
+  it("parses workout with just Name field", () => {
+    expect(parse(`Name: My Workout`)).toMatchInlineSnapshot(`
+      Object {
+        "author": "",
+        "description": "",
+        "intervals": Array [],
+        "name": "My Workout",
+      }
+    `);
+  });
+
+  it("parses workout header with all fields", () => {
+    expect(
+      parse(`Name: My Workout
+Author: John Doe
+Description:
+  It's a great workout.
+  
+  Do it when you dare,
+  it'll cause lots of pain.
+`)
+    ).toMatchInlineSnapshot(`
+      Object {
+        "author": "John Doe",
+        "description": "It's a great workout.
+
+      Do it when you dare,
+      it'll cause lots of pain.",
+        "intervals": Array [],
+        "name": "My Workout",
+      }
+    `);
+  });
 });
