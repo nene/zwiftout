01d49f45c3ce3f056835bc3d713ab541278f1c2a
Rene Saarsoo <nene@triin.net>
2020-09-24 11:13:49 +0300

    Fix duration sum calculation

--- src/stats/totalDuration.ts
+++ src/stats/totalDuration.ts
@@ -1,5 +1,6 @@
-import { pluck, sum } from "ramda";
+import { map, sum } from "ramda";
 import { Interval } from "../ast";
 import { Seconds } from "../types";
 
-export const totalDuration = (intervals: Interval[]): Seconds => new Seconds(sum(pluck("duration.value", intervals)));
+export const totalDuration = (intervals: Interval[]): Seconds =>
+  new Seconds(sum(map((i) => i.duration.value, intervals)));
