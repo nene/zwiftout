49638c6c7129d425b49ed0b3de15f8f7f959a4b4
Rene Saarsoo <nene@triin.net>
2020-09-30 22:21:40 +0300

    Regression tests for stats and ZWO creation

---
+++ src/test/cli.test.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+import { generateZwo } from "../generateZwo";
+import { parse } from "../parser";
+import { stats } from "../stats";
+
+const createStats = (filename: string) => stats(parse(fs.readFileSync(filename, "utf8")));
+const createZwo = (filename: string) => generateZwo(parse(fs.readFileSync(filename, "utf8")));
+
+const filenames = [
+  "examples/comments.txt",
+  "examples/darth-vader.txt",
+  "examples/halvfems.txt",
+  "examples/threshold-pushing.txt",
+];
+
+describe("Generate ZWO", () => {
+  filenames.forEach((filename) => {
+    it(filename, () => {
+      expect(createZwo(filename)).toMatchSnapshot();
+    });
+  });
+});
+
+describe("Generate stats", () => {
+  filenames.forEach((filename) => {
+    it(filename, () => {
+      expect(createStats(filename)).toMatchSnapshot();
+    });
+  });
+});
