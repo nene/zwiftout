c9e71dc2443433b18688dcfb3ba33444f9f09108
Rene Saarsoo <nene@triin.net>
2020-10-01 22:41:04 +0300

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export { generateZwo } from "./generateZwo";
 export { parse } from "./parser";
 export { stats } from "./stats";
 export { parseCliOptions } from "./parseCliOptions";
+
+export { Workout, Interval, Comment } from "./ast";
+export { Duration } from "./Duration";
+export { Intensity, IntensityRange, FreeIntensity } from "./Intensity";
