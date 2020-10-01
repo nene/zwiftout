14930e748e91bc2804934b4338ff6d9569700c05
Rene Saarsoo <nene@triin.net>
2020-10-01 23:32:29 +0300

    Export totalDuration()

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export { parse } from "./parser";
 export { stats } from "./stats";
 export { parseCliOptions } from "./parseCliOptions";
 
+// types
 export { Workout, Interval, Comment } from "./ast";
 export { Duration } from "./Duration";
 export { Intensity, IntensityRange, FreeIntensity } from "./Intensity";
+
+// stats utils
+export { totalDuration } from "./stats/totalDuration";
